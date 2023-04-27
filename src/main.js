4bb7890690cb3b3eea55dff04d3acf92c50d299d
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2023-04-27 15:44:39 -0700

    added more mods

--- src/main.js
+++ src/main.js
@@ -6,10 +6,20 @@
         - Display the time remaining (in seconds) on the screen (10)
         - Implement a new timing/scoring mechanism that adds time to the clock for successful hits (15)
         - Create 4 new explosion sound effects and randomize which one plays on impact (10)
+        - Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (15)
+        - Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (15)
+        - Implement the speed increase that happens after 30 seconds in the original game (5)
+        - Allow the player to control the Rocket after it's fired (5)
+        - Create a new title screen (e.g., new artwork, typography, layout) (10)
+        - Add your own (copyright-free) background music to the Play scene (please be mindful of the volume) (5)
         - 
+
+
     
     SOURCES:
         - https://stackoverflow.com/questions/9071573/is-there-a-simple-way-to-make-a-random-selection-from-an-array-in-javascript-or
+        - https://labs.phaser.io/edit.html?src=src/game%20objects/particle%20emitter/explode%20emitter.js
+        - https://blog.ourcade.co/posts/2020/phaser-3-web-audio-best-practices-games/
         - 
 
 */
